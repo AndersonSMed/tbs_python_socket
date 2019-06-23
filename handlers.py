72734bcbdc2597790e2e383af873c37eb667acbe
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-06-23 00:50:12 -0300

    Resolvido alguns bugs quando o usuário relogava

--- handlers.py
+++ handlers.py
@@ -1,27 +1,44 @@
 import socketio
-import json
+import asyncio
 
 class Game(socketio.AsyncNamespace):
     
     _players = list()
 
-    def on_enter(self, sid, nickname):
-        self._players.append({
-            'nickname': str(nickname),
-            'sid': str(sid)
-        })
+    async def on_enter(self, sid, nickname):
+        playerFound = False
+        for index in range(0, len(self._players)):
+            
+            if str(sid) == self._players[index]['sid']:
+                self._players[index] = {
+                    'nickname': str(nickname),
+                    'sid': str(sid)
+                }
+                playerFound = True
+                break
+
+        if not playerFound:
+            
+            self._players.append({
+                'nickname': str(nickname),
+                'sid': str(sid)
+            })
+
         new_message = {
             'player': 'Servidor',
             'message': 'Um novo companheiro se junta ao lobby, deem boas vindas ao ' + str(nickname)
         }
-        self.emit('new_message', data = json.dumps(new_message))
+        
+        await self.emit('new_message', data = new_message)
 
-    def on_send_message(self, sid, message):
+        await self.emit('info', data = 'Seja bem vindo, ' + str(nickname) + ', esperamos que você se divirta', room = sid)
+
+    async def on_send_message(self, sid, message):
         for player in self._players:
-            if str(sid) == player.sid:
+            if str(sid) == player['sid']:
                 new_message = {
                     'player': player['nickname'],
                     'message': str(message)
                 }
-                self.emit('new_message', json.dumps(new_message))
+                await self.emit('new_message', new_message)
                 break
--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from aiohttp import web
 import socketio
+import handlers
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 
 app = web.Application()
 
 sio.attach(app)
+
+sio.register_namespace(handlers.Game('/game'))
 
 if __name__ == '__main__':
     web.run_app(app, port = 8000)
